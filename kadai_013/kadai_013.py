0db179c20ea57e1b0b10053720308c99f66b7d19
marupon3910 <96562149+marupon3910@users.noreply.github.com>
2023-09-26 11:43:27 +0900

    kadai_013.pyを更新

--- kadai_013/kadai_013.py
+++ kadai_013/kadai_013.py
@@ -1,8 +1,5 @@
 #購入金額をtotal、商品の金額をprice、消費税をtaxと定義して作成
 def total(price,tax):
   total = price * (1 +(tax / 100))
-  #変数$totalの値を出力する
-  print(f"{total}円")
-
-#関数を呼び出し、引数として購入金額と消費税を渡す
-#total(1000,10)
+  return f"{total}円"
+print(total(1000,10))
